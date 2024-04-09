0132fd34bc314a412899a6d1599abae617dadd0e
Roga <ryanroga@gmail.com>
2024-04-09 12:03:22 -0300

    Install Vue Shadcn

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@formkit/auto-animate/nuxt', '@nuxtjs/robots', 'nuxt-primevue'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@formkit/auto-animate/nuxt', '@nuxtjs/robots', 'nuxt-primevue'],
   tailwindcss: {
     // Options
   },
   robots: {
     // Options
+  },
+  shadcn: {
+    prefix: 'U',
+    componentDir: './components'
   }
 });
