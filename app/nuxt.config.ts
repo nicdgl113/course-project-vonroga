0d52d7717f715f6cac9fbe9187aef6820094acf7
Roga <ryanroga@gmail.com>
2024-04-09 12:36:37 -0300

    add nuxt-icon

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@formkit/auto-animate/nuxt',],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@formkit/auto-animate/nuxt',
+    'nuxt-icon',],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
