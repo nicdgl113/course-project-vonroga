9df5d1fa75b03b44e348f4b1cfcaaf0d99f8a68b
Roga <ryanroga@gmail.com>
2024-04-09 11:59:22 -0300

    Adds Robots

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@formkit/auto-animate/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@formkit/auto-animate/nuxt', '@nuxtjs/robots',],
   tailwindcss: {
     // Options
   },
+  robots: {
+    // Options
+  }
 });
