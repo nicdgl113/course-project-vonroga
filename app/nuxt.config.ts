4a53a1584761ec74eac62ffb3abd5af623c1ba69
Roga <ryanroga@gmail.com>
2024-04-09 12:35:15 -0300

    add auto-animate

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt',],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@formkit/auto-animate/nuxt',],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
