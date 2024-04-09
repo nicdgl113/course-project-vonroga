8d1780a06cbe08004f1fdab013272a8e16406b77
Roga <ryanroga@gmail.com>
2024-04-09 11:34:53 -0300

    Tailwind Config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-
+  modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    // Options
+  },
 });
